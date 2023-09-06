f66b06600cfdb5d048d11b75de2b719590e0ce9c
morbius-os <dogankaan217@gmail.com>
2023-09-06 17:34:41 +0300

    all-pip-installer

---
+++ all-pip-installer/all-pip.py
@@ -0,0 +1,330 @@
+#!/usr/bin/env python3
+#*- coding: utf-8 -*--
+#Coded by Morbius.os
+
+AUTHOR = 'Morbius.os'
+GİTHUB = 'https://github.com/morbius-os'
+INSTAGRAM= '@morbius.os'
+
+import os
+import time
+import sys
+
+Mor = '\033[95m'
+Cyan = '\033[96m'
+KoyuMavi = '\033[1;34m'
+Mavi = '\033[94m'
+Yeşil = '\033[92m'
+Sarı = '\033[93m'
+Kırmızı = '\033[91m'
+Kalın = '\033[1m'
+AltıÇizili = '\033[4m'
+Bitir = '\033[0m'
+Beyaz ='\033[1;37m'
+
+
+def animasyon(yazi):
+     for u in yazi + '\n' :
+         sys.stdout.write(u)
+         sys.stdout.flush()
+         time.sleep(10. / 100)
+         
+animasyon(f'''{Beyaz}Bu Script {Yeşil}Morbius.os{Beyaz} Tarafından Yazılmıştır.
+Bütün Python Kütüphanelerini Otomatik Olarak Cihazınıza Yükler.
+İşlem Biraz Zaman Alabilir.{Beyaz}''')
+time.sleep(2)
+
+os.system('pip install mechanize')
+print(f"{Yeşil}mechanize{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install requests')
+print(f"{Yeşil} requests{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install pyfiglet')
+print(f"{Yeşil}pyfiglet{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install sys')
+print(f"{Yeşil}sys{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install py_compile')
+print(f"{Yeşil}py_compile{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install youtube_dl')
+print(f"{Yeşil}youtube_dl{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install uuid')
+print(f"{Yeşil}uuid{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install time')
+print(f"{Yeşil}time{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install os')
+print(f"{Yeşil}os{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install random')
+print(f"{Yeşil}random{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install datetime')
+print(f"{Yeşil}datetime{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install string')
+print(f"{Yeşil}string{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install secrets')
+print(f"{Yeşil}secrets{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install webbrowser')
+print(f"{Yeşil}webbrowser{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install hashlib')
+print(f"{Yeşil}hashlib{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install colorama')
+print(f"{Yeşil}colorama{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install n')
+print(f"{Yeşil}n{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install mm')
+print(f"{Yeşil}mm{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install new')
+print(f"{Yeşil}new{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install sleep')
+print(f"{Yeşil}sleep{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install BeautifulSoup')
+print(f"{Yeşil}BeautifulSoup{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install new')
+print(f"{Yeşil}new{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install pafy')
+print(f"{Yeşil}pafy{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install bs4')
+print(f"{Yeşil}bs4{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install sys')
+print(f"{Yeşil}sys{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install json')
+print(f"{Yeşil}json{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install threading')
+print(f"{Yeşil}threading{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install render')
+print(f"{Yeşil}render{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install cfonts')
+print(f"{Yeşil}cfonts{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install random')
+print(f"{Yeşil}random{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install uuid')
+print(f"{Yeşil}uuid{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install secrets')
+print(f"{Yeşil}secrets{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install datetime')
+print(f"{Yeşil}datetime{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install *')
+print(f"{Yeşil}*{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install argparse')
+print(f"{Yeşil}argprase{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install InstagramAPI')
+print(f"{Yeşil}InstagramAPI{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install sleep')
+print(f"{Yeşil}sleep{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install string')
+print(f"{Yeşil}string{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install uuid4')
+print(f"{Yeşil}uuid4{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install generate_user_agent')
+print(f"{Yeşil}generate_user_agent{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install threading')
+print(f"{Yeşil}threading{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install json')
+print(f"{Yeşil}json{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install datetime')
+print(f"{Yeşil}datetime{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install token_hex')
+print(f"{Yeşil}token_hex{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install secrets')
+print(f"{Yeşil}secrets{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install Fore')
+print(f"{Yeşil}Fore{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+os.system('pip install re')
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+print(f"{Yeşil}re{Beyaz} Kütüphanesi Yüklendi.")
+os.system('pip install b')
+print(f"{Yeşil}b{Beyaz} Kütüphanesi Yüklendi.")
+try:
+    os.system('clear')
+except:
+    os.system('cls')
+
+print(f"{Yeşil}Kütüphaneler Cihazınıza Yüklenmiştir.")
